53d67c31e934070aabf07bd95390ab124f7811ec
Russell Gordon <rgordon@rsgc.on.ca>
2016-04-12 09:57:14 -0400

    Initial Commit

---
+++ quadratic-solver/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  quadratic-solver
+//
+//  Created by Russell Gordon on 2016-04-12.
+//  Copyright © 2016 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
